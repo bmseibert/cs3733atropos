b7af88b83a0d4a194a4a4d67d4d026e20097afba
erjoh <erjoh@192.168.1.231>
2019-11-20 19:02:39 -0500

    Added Segment

--- src/main/java/model/Segment.java
+++ src/main/java/model/Segment.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Segment {
+	public final String name;
+	public final String character;
+	public final String url;
+	public Boolean isMarked;
 	
+	public Segment(String name, String character, String url, Boolean isMarked) {
+		this.name = name;
+		this.character = character;
+		this.url = url;
+		this.isMarked = isMarked;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getCharacter() {
+		return this.character;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+	
+	public Boolean getIsMarked() {
+		return this.isMarked;
+	}
+	
+	public void setIsMarked(Boolean b) {
+		this.isMarked = b;
+	}
+	
+	public void changeMarkedStatus() {
+		this.isMarked = !this.isMarked;
+	}
+	
+	public boolean equals (Object o) {
+		if (o == null) { return false; }
+		
+		if (o instanceof Segment) {
+			Segment other = (Segment) o;
+			return name.equals(other.name);
+		}
+		
+		return false;  // not a Constant
+	}
 }
