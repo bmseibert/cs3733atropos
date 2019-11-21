6f04f5842a52ea9ba55a9fd6b1efe224ec84eddf
sebastien <sebastien@sebastien-Lenovo-ideapad-300-17ISK>
2019-11-20 20:03:15 -0500

    Worked on Playlist model

--- src/main/java/model/Playlist.java
+++ src/main/java/model/Playlist.java
@@ -1,5 +1,25 @@
 package model;
+import java.util.ArrayList;
 
 public class Playlist {
+	public final String name;
+	
+	public Playlist(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void appendVideoSegment(Segment seg) {
+		ArrayList<Segment> segments = new ArrayList<Segment>();
+		segments.add(seg);
+	}
+	
+	public void removeVideoSegment(Segment seg) {
+		ArrayList<Segment> segments = new ArrayList<Segment>();
+		segments.remove(seg);
+	}
 
 }
