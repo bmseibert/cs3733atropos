7fc7a6adaa07509a5e287ef50128da57160e6cff
sebastien <sebastien@sebastien-Lenovo-ideapad-300-17ISK>
2019-11-22 14:59:38 -0500

    Worked on http Responses

--- src/main/java/atropos/http/CreatePlaylistResponse.java
+++ src/main/java/atropos/http/CreatePlaylistResponse.java
@@ -1,5 +1,26 @@
 package atropos.http;
 
 public class CreatePlaylistResponse {
+	public String name;
+	public int statusCode;
+	public String error;
+	
+	public CreatePlaylistResponse(String name, int code) {
+		this.name = name;
+		this.statusCode = code;
+		this.error = "";
+	}
+	
+	public CreatePlaylistResponse(String name, int statusCode, String errorMessage) {
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+		this.name = name;
+	}
+	
+	public String toString() {
+		return "Response(" + error + ")";
+	}
+	
+	
 
 }
--- src/main/java/atropos/http/DeletePlaylistResponse.java
+++ src/main/java/atropos/http/DeletePlaylistResponse.java
@@ -1,5 +1,27 @@
 package atropos.http;
 
 public class DeletePlaylistResponse {
-
+	public final String name;
+	public final int statusCode;
+	public final String error;
+	
+	public DeletePlaylistResponse(String name, int code) {
+		this.name = name;
+		this.statusCode = code;
+		this.error = "";
+	}
+	
+	public DeletePlaylistResponse(String name, int code, String errorMessage) {
+		this.name = name;
+		this.statusCode = code;
+		this.error = errorMessage;
+	}
+	
+	public String toString() {
+		if (statusCode / 100 == 2) { 
+			return "DeleteResponse(" + name + ")";
+		} else {
+			return "ErrorResult(" + name + ", statusCode=" + statusCode + ", err=" + error + ")";
+		}
+	}
 }
