020d9a1a1e0c56907e746cabb25cdf8e751641e1
bmseibert <bmseibert@wpi.edu>
2019-11-22 20:26:19 -0500

    Added Requests classes and consolidated them

--- src/main/java/atropos/http/ChangeMarkVideoRequest.java
+++ src/main/java/atropos/http/ChangeMarkVideoRequest.java
@@ -1,18 +1,24 @@
 package atropos.http;
 
 /** To work with AWS must not have final attributes, must have no-arg constructor, and all get/set methods. */
-public class DeleteVideoSegmentRequest {
+public class ChangeMarkVideoRequest {
 
 	String segment;
+	Boolean isMarked;
 	
-	public DeleteVideoSegmentRequest() {}
+	public ChangeMarkVideoRequest() {}
 	
-	public DeleteVideoSegmentRequest(String segment)
+	public ChangeMarkVideoRequest(String segment, Boolean isMarked)
 	{
 		this.segment = segment;
+		this.isMarked = isMarked;
 	}
 	
 	public void setSegment(String newSegment) {this.segment = newSegment;}
-	
 	public String getSegment() {return segment;}
+	
+	public void setMarked(Boolean newStatus) {this.isMarked = newStatus;}
+	public Boolean getMarked() {return isMarked;}
+	
+	
 }
--- src/main/java/atropos/http/UnmarkSegmentRequest.java
+++
@@ -1,5 +0,0 @@
-package atropos.http;
-
-public class UnmarkSegmentRequest {
-
-}
