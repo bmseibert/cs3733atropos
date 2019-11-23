f1f62d4eff7f0bbc32948026aaff21d6115c92ee
bmseibert <bmseibert@wpi.edu>
2019-11-22 19:32:35 -0500

    Started adding request bodies

--- src/main/java/atropos/http/AddPlaylistRequest.java
+++ src/main/java/atropos/http/AddPlaylistRequest.java
@@ -1,5 +1,20 @@
 package atropos.http;
 
+/** To work with AWS must not have final attributes, must have no-arg constructor, and all get/set methods. */
 public class AddPlaylistRequest {
 
+	String playlistName;
+	
+	public AddPlaylistRequest() {}
+	
+	public AddPlaylistRequest(String playlistName) 
+	{
+		this.playlistName = playlistName;
+	}
+	
+	public void setPlaylistName(String newName) {this.playlistName = newName;}
+	
+	public String getPlaylistName() {return playlistName;}
+	
 }
+
