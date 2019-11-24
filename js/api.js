e9cecf5f044f3991c7503e8f689869db26236f8d
erjoh <erjoh@192.168.1.231>
2019-11-24 18:52:36 -0500

    Updated Api

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = ""; 
+var restApi = "y2b4rjh4x5";
+var awsRegion = "us-east-1";
+var base_url = "https://"+ restApi + ".execute-api." + AwsRegion +".amazonaws.com/"; 
 
-var list_segment_url    = base_url + "";   // POST
+var list_segment_url    = base_url + "segments";   // POST
