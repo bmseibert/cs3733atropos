d7eb04620923bdea408f9e096e652409290871ac
Your Name <riverstone98@gmail.com>
2019-11-25 17:40:58 -0500

    show playlists help

--- js/showPlaylists.js
+++ js/showPlaylists.js
@@ -16,35 +16,32 @@ function processListPlaylistsResponse(result){
 	        var playlistJson = playlists[i];	        
 	        var name = playlistJson["name"];
 	        
-	        output = output + "<div id=\"playlist" + name + "\"><b>" + name + ":</b> </div>";
+	        output = output + "<div id=\"segment" + name + "\"><b>" + name + ":</b><br></div>";
 	        
-	        
-	        //<video id="num0" width="320" height="240" controls>
-//	        <source src="" type="video/ogg">
-//	        Your browser does not support the video tag.
-//	        </video>
 	      }
-	    playlistList.innerHTML = output
+	    playlistList.innerHTML = output;
 
 	  } else {
 	    var msg = "error";
 	  }
 }
 
-function handleShowSegmentsClick(e){
+function handleShowPlaylistsClick(e){
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", list_playlist_url, true);
 	
+	// send the collected data as JSON
+	xhr.send(null);
 	
 	xhr.onloadend = function () {
 		console.log(xhr);
 	    console.log(xhr.request);
-	    
-	    if (xhr.readyState == XMLHttpRequest.DONE) {
-	        console.log ("XHR:" + xhr.responseText);
-	        processListPlaylistsResponse(xhr.responseText);
-	      } else {
-	    	processListPlaylistsResponse("N/A");
-		      }
-		}
+	    playlistList.innerHTML = "<div> TESTTTT </div>"; 
+	   // if (xhr.readyState == XMLHttpRequest.DONE) {
+	    //    console.log ("XHR:" + xhr.responseText);
+	    //    processListPlaylistsResponse(xhr.responseText);
+	     // } else {
+	    //	processListPlaylistsResponse("N/A");
+		 //     } 
+	//	}
 	}
