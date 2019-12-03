93ef644c904d64b9907873da4626ffaa6597e8b4
erjoh <erjoh@192.168.1.231>
2019-12-02 21:55:14 -0500

    Made both Mark Segment and Unmark

--- src/main/java/atropos/videolibraryapp/MarkSegementHandler.java
+++ src/main/java/atropos/videolibraryapp/MarkSegementHandler.java
@@ -1,5 +1,55 @@
 package atropos.videolibraryapp;
 
-public class MarkSegementHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import atropos.videolibraryapp.db.SegmentsDAO;
+import atropos.videolibraryapp.http.ChangeMarkVideoRequest;
+import atropos.videolibraryapp.http.MarkSegmentResponse;
+import atropos.videolibraryapp.model.Segment;
+
+public class MarkSegementHandler implements RequestHandler<ChangeMarkVideoRequest, MarkSegmentResponse>{
+	
+	LambdaLogger logger;
+	
+	// helpers
+	boolean unmarkVideoSegment(Segment segment) throws Exception {
+		if (logger != null) { 
+			logger.log("in unmarkVideoSegment"); 
+		}
+		SegmentsDAO dao = new SegmentsDAO(System.getenv("DB_url"),System.getenv("DB_name"),System.getenv("DB_password"));
+		dao.updateSegmentMark(segment);
+		return true;
+	}
+	
+	@Override
+	public MarkSegmentResponse handleRequest(ChangeMarkVideoRequest cmvr, Context context) {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler of RequestHandler");
+		
+		boolean fail = false;
+		String failMessage = "";
+		String successMessage = "";
+		String segmentName = cmvr.getSegment();
+		Segment newSeg = new Segment(segmentName);
+		newSeg.toggleIsMarked();
+
+		//logic
+		try {
+			unmarkVideoSegment(newSeg);
+		}catch (Exception e){
+			fail = true;
+			failMessage = "Failed to update marked status";
+		}
+		
+		MarkSegmentResponse usr;
+		if(fail) {
+			usr = new MarkSegmentResponse(400, failMessage);
+		}else {
+			usr = new MarkSegmentResponse(successMessage, 200);
+		}
+		return usr;
+	}
 
 }
--- src/main/java/atropos/videolibraryapp/UnmarkSegementHandler.java
+++ src/main/java/atropos/videolibraryapp/UnmarkSegementHandler.java
@@ -1,5 +1,54 @@
 package atropos.videolibraryapp;
 
-public class UnmarkSegementHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import atropos.videolibraryapp.db.SegmentsDAO;
+import atropos.videolibraryapp.http.ChangeMarkVideoRequest;
+import atropos.videolibraryapp.http.UnmarkSegmentResponse;
+import atropos.videolibraryapp.model.Segment;
+
+public class UnmarkSegementHandler implements RequestHandler<ChangeMarkVideoRequest, UnmarkSegmentResponse>{
+	
+	LambdaLogger logger;
+	
+	// helpers
+	boolean unmarkVideoSegment(Segment segment) throws Exception {
+		if (logger != null) { 
+			logger.log("in unmarkVideoSegment"); 
+		}
+		SegmentsDAO dao = new SegmentsDAO(System.getenv("DB_url"),System.getenv("DB_name"),System.getenv("DB_password"));
+		dao.updateSegmentMark(segment);
+		return true;
+	}
+	
+	@Override
+	public UnmarkSegmentResponse handleRequest(ChangeMarkVideoRequest cmvr, Context context) {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler of RequestHandler");
+		
+		boolean fail = false;
+		String failMessage = "";
+		String successMessage = "";
+		String segmentName = cmvr.getSegment();
+		Segment newSeg = new Segment(segmentName);
+
+		//logic
+		try {
+			unmarkVideoSegment(newSeg);
+		}catch (Exception e){
+			fail = true;
+			failMessage = "Failed to update marked status";
+		}
+		
+		UnmarkSegmentResponse usr;
+		if(fail) {
+			usr = new UnmarkSegmentResponse(400, failMessage);
+		}else {
+			usr = new UnmarkSegmentResponse(successMessage, 200);
+		}
+		return usr;
+	}
 
 }
