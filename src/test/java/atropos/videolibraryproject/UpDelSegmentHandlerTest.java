f770258382fc3e036f9e59a442fd68132aaf299c
erjoh <erjoh@192.168.1.231>
2019-12-03 01:53:53 -0500

    DeleteSegement works fine, upload segment fails, working on fix

---
+++ src/test/java/atropos/videolibraryproject/UpDelSegmentHandlerTest.java
@@ -0,0 +1,88 @@
+package atropos.videolibraryproject;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+import atropos.videolibraryapp.DeleteSegmentHandler;
+import atropos.videolibraryapp.UploadVideoSegmentHandler;
+import atropos.videolibraryapp.http.DeleteVideoSegmentRequest;
+import atropos.videolibraryapp.http.DeleteVideoSegmentResponse;
+import atropos.videolibraryapp.http.UploadVideoSegmentRequest;
+import atropos.videolibraryapp.http.UploadVideoSegmentResponse;
+
+
+public class UpDelSegmentHandlerTest extends LambdaTest {
+	
+	void testSuccessDeleteInput(String incoming, String outgoing) throws IOException {
+    	DeleteSegmentHandler handler = new DeleteSegmentHandler();
+    	DeleteVideoSegmentRequest req = new Gson().fromJson(incoming, DeleteVideoSegmentRequest.class);
+       
+    	DeleteVideoSegmentResponse resp = handler.handleRequest(req, createContext("create"));
+    	
+    	Assert.assertEquals(outgoing, resp.name);
+        Assert.assertEquals(200, resp.statusCode);
+    }
+	
+    void testFailDeleteInput(String incoming, String outgoing) throws IOException {
+    	DeleteSegmentHandler handler = new DeleteSegmentHandler();
+    	DeleteVideoSegmentRequest req = new Gson().fromJson(incoming, DeleteVideoSegmentRequest.class);
+
+    	DeleteVideoSegmentResponse resp = handler.handleRequest(req, createContext("create"));
+    	
+        Assert.assertEquals(400, resp.statusCode);
+    }
+    
+	void testSuccessUploadInput(String incoming, String outgoing) throws IOException {
+    	UploadVideoSegmentHandler handler = new UploadVideoSegmentHandler();
+    	UploadVideoSegmentRequest req = new Gson().fromJson(incoming, UploadVideoSegmentRequest.class);
+       
+    	UploadVideoSegmentResponse resp = handler.handleRequest(req, createContext("create"));
+    	
+    	// Assert.assertEquals(outgoing, resp.name);
+        Assert.assertEquals(200, resp.statusCode);
+    }
+	
+    void testFailUploadInput(String incoming, String outgoing) throws IOException {
+    	UploadVideoSegmentHandler handler = new UploadVideoSegmentHandler();
+    	UploadVideoSegmentRequest req = new Gson().fromJson(incoming, UploadVideoSegmentRequest.class);
+
+    	UploadVideoSegmentResponse resp = handler.handleRequest(req, createContext("create"));
+    	
+        Assert.assertEquals(400, resp.statusCode);
+    }
+    
+    
+    
+    @Test
+    public void testUploadSegment() {
+    	
+    	String input = "{\"segment\": \"We move together\"}";
+    	String RESULT = "Success";
+    	
+    	try {
+        	testSuccessUploadInput(input, RESULT);
+        } catch (IOException ioe) {
+        	Assert.fail("Invalid:" + ioe.getMessage());
+        }
+    	
+    }
+    
+    @Test
+    public void testDeleteSegment() {
+    	
+    	String input = "{\"segment\": \"We move together\"}";
+    	String RESULT = "Success";
+    	
+    	try {
+        	testSuccessDeleteInput(input, RESULT);
+        } catch (IOException ioe) {
+        	Assert.fail("Invalid:" + ioe.getMessage());
+        }
+    	
+    }
+	
+}
