6bbbbdd248330fd0e64ce07661e8f94c743e1c5e
erjoh <erjoh@192.168.1.231>
2019-12-04 18:42:50 -0500

    API.js file is fully written

--- js/api.js
+++ js/api.js
@@ -3,7 +3,24 @@
 
 var base_url = "https://y2b4rjh4x5.execute-api.us-east-1.amazonaws.com/Beta/"; 
 
-var list_segment_url    = base_url + "segments";   // POST
-var list_playlist_url  = base_url + "playlists"; // GET
+var create_playlist_url = base_url + "playlist"; // POST
+	var append_segment_url = create_playlist_url + "/" + "appendSegment"; // POST
+	var delete_playlist_url = create_playlist_url + "/" + "delete"; // POST
+	var remove_segment_url = create_playlist_url + "/" + "removeSegment"; // POST
+
+var list_playlist_url = base_url + "playlists"; // GET
+
+var remote_url = base_url + "remote"; 
+	var list_sites_url = remote_url + "/" + "listSites"; // GET
+	var register_site = remote_url + "/" + "registerSite"; // POST
+	var un_register_site = remote_url + "/" + "unregisterSite"; // POST
 
 var upload_segment_url = base_url + "segment"; // POST
+	var delete_segment_url = upload_segment_url + "/" + "delete"; // POST
+	var mark_segment_url = upload_segment_url + "/" + "mark"; // POST
+	var unmark_segment_url = upload_segment_url + "/" + "unmark"; // POST
+
+var list_segment_url = base_url + "segments";   // POST
+	var search_url = list_segment_url + "/" + "search"; // POST
+
+
