7f8a63255903495fc3be754f892f89ac7f74239c
erjoh <erjoh@192.168.1.231>
2019-12-04 20:29:47 -0500

    Added alert for playlist name needing to be not empty

--- js/createPlaylist.js
+++ js/createPlaylist.js
@@ -13,30 +13,37 @@ function handleCreatePlaylistClick(e){
 	var form = document.createPlaylist
 	
 	var data = {};
+	var playName = form.playlist.value
 	data["playlistName"] = form.playlist.value;
 	
-	var js = JSON.stringify(data);
-	console.log("JS:" + js);
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", create_playlist_url, true);
-	
-	// send the collected data as JSON
-	xhr.send(js);
-	
-	xhr.onloadend = function () {
-		console.log(xhr);
-	    console.log(xhr.request);
-	    if(xhr.readyState == XMLHttpRequest.DONE){
-	    	var js = JSON.parse(xhr.responseText);
-	    	var statusCode = js["statusCode"];
-	    	
-		    if(statusCode == 200){
-		    	handleSuccessPlaylistCreate();
-		    }else{
-		    	handleFailedPlaylistCreate();
+	if(playName != ""){
+		var js = JSON.stringify(data);
+		console.log("JS:" + js);
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", create_playlist_url, true);
+		
+		// send the collected data as JSON
+		xhr.send(js);
+		
+		xhr.onloadend = function () {
+			console.log(xhr);
+		    console.log(xhr.request);
+		    if(xhr.readyState == XMLHttpRequest.DONE){
+		    	var js = JSON.parse(xhr.responseText);
+		    	var statusCode = js["statusCode"];
+		    	
+			    if(statusCode == 200){
+			    	handleSuccessPlaylistCreate();
+			    }else{
+			    	handleFailedPlaylistCreate();
+			    }
 		    }
-	    }
-	    
-		}
+		    
+			}
+	}else{
+		alert("Playlist name cannot be empty");
+	}
+	
+	
 
 }
