22f83321aa4d7daeee2689e78c6668823fcef7f7
scasimir <scasimir@wpi.edu>
2019-12-04 20:31:53 -0500

    Added filter function to search and added delete playlist button

--- js/search.js
+++ js/search.js
@@ -5,3 +5,20 @@
 function handleSearchSegments(){
 	
 }
+
+function handleFilterSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
