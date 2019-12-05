a8c7c365dd62952aea5c683dad42efa3160244a4
erjoh <erjoh@192.168.1.231>
2019-12-05 15:22:41 -0500

    Files Fixed

--- src/main/java/atropos/videolibraryapp/ListPublicVideoSegments.java
+++ src/main/java/atropos/videolibraryapp/ListPublicVideoSegments.java
@@ -1,11 +1,16 @@
 package atropos.videolibraryapp;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import atropos.videolibraryapp.db.SegmentsDAO;
 import atropos.videolibraryapp.http.EmptyRequest;
 import atropos.videolibraryapp.http.PublicVideoSegmentResponse;
 import atropos.videolibraryapp.model.Segment;
@@ -14,9 +19,12 @@ public class ListPublicVideoSegments implements RequestHandler<EmptyRequest, Pub
 	
 	LambdaLogger logger;
 	//helpers
-	boolean getAllPublicSegments() throws Exception{
-		
-		return true;
+	ArrayList<Segment> getAllPublicSegments() throws Exception{
+		if (logger != null) { 
+			logger.log("in getAllPublicSegments"); 
+		}
+		SegmentsDAO dao = new SegmentsDAO((String)System.getenv("DB_url"),(String)System.getenv("DB_name"),(String)System.getenv("DB_password"));
+		return dao.getAllSegmentsByMarkRemote(false, false);
 	}
 	
 	@Override
@@ -28,17 +36,26 @@ public class ListPublicVideoSegments implements RequestHandler<EmptyRequest, Pub
 		
 		//logic
 		try {
-			
+			list = getAllPublicSegments();
 		}catch(Exception e) {
 			fail = true;
 			failMessage = "Could not connect";
 		}
 		
+		
+		
+//		// parsing file "JSONExample.json" 
+//        Object obj = new JSONParser().parse(list); 
+//          
+//        // typecasting obj to JSONObject 
+//        JSONObject jo = (JSONObject) obj; 
+//		
+		
 		PublicVideoSegmentResponse pvsr;
 		if(fail) {
-			pvsr = new PublicVideoSegmentResponse(list, 200);
-		}else {
 			pvsr = new PublicVideoSegmentResponse(400, failMessage);
+		}else {
+			pvsr = new PublicVideoSegmentResponse(list, 200);
 		}
 		
 		return pvsr;
